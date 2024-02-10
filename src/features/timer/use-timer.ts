1a8d611960b5db85984e13469581a9dae9c9df7e
yoshikouki <yoshikouki@gmail.com>
2024-02-10 19:54:22 +0900

    Clean useTimer

--- src/features/timer/use-timer.ts
+++ src/features/timer/use-timer.ts
@@ -3,45 +3,24 @@ import { useState } from "react";
 import { pomodoro } from "./pomodoro";
 import { useInterval } from "./use-interval";
 
-export const useTimer = () => {
+type Props = {
+  delay?: number;
+};
+
+export const useTimer = (props?: Props) => {
+  const delay = props?.delay || 1000;
+
   const [remainingSeconds, setRemainingSeconds] = useState(
     pomodoro.detectRemainingSeconds()
   );
-  const [elapsedSeconds, setElapsedSeconds] = useState(0);
-
-  const [isRunning, setIsRunning] = useState(false);
-  const [delay, setDelay] = useState(1000);
-
-  const start = () => {
-    setIsRunning(true);
-  };
-
-  const stop = () => {
-    setIsRunning(false);
-  };
-
-  const reset = () => {
-    setRemainingSeconds(0);
-    setElapsedSeconds(0);
-  };
-
-  const setDelayValue = (value: number) => {
-    setDelay(value);
-  };
 
   useInterval(() => {
-    setRemainingSeconds((prev) => prev - 1);
-    setElapsedSeconds((prev) => prev + 1);
-  }, 1000);
+    setRemainingSeconds(pomodoro.detectRemainingSeconds());
+  }, delay);
 
   return {
     remainingSeconds,
-    elapsedSeconds,
-    start,
     stop,
-    reset,
-    isRunning,
     delay,
-    setDelayValue,
   };
 };
