228fcf6ba0f4f73f90ba377e7ab4255a80bbf5bd
yoshikouki <yoshikouki@gmail.com>
2024-02-10 12:43:12 +0900

    Does not lose style at tick

--- src/features/timer/time-display.tsx
+++ src/features/timer/time-display.tsx
@@ -6,7 +6,7 @@ export const TimeDisplay = ({ seconds }: Props) => {
   const minutes = Math.floor(seconds / 60);
   const displaySeconds = (seconds % 60).toString().padStart(2, "0");
   return (
-    <div className="font-black text-9xl">
+    <div className="tabular-nums font-black text-9xl">
       {minutes}:{displaySeconds}
     </div>
   );
