a5f13188595dd1cc7791c0bc2c5a79c81d1cb9ba
yoshikouki <yoshikouki@gmail.com>
2024-02-11 16:53:37 +0900

    Progress bar of text

--- src/features/timer/time-display.tsx
+++ src/features/timer/time-display.tsx
@@ -4,11 +4,41 @@ type Props = {
 
 export const TimeDisplay = ({ seconds }: Props) => {
   const minutes = Math.floor(seconds / 60);
-  const displaySeconds = (seconds % 60).toString().padStart(2, "0");
+  const percentage = (seconds % 60) * (100 / 60);
+
   return (
-    <div className="flex justify-center items-baseline tabular-nums font-black">
-      <div className="text-9xl">{minutes}</div>
-      <div className="text-5xl text-muted">{displaySeconds}</div>
+    <div className="flex justify-center items-center h-screen">
+      <svg width="0" height="0">
+        <defs>
+          <clipPath id="clipPath">
+            <text
+              x="50%"
+              y="50%"
+              textAnchor="middle"
+              dominantBaseline="middle"
+              fontSize="120"
+              fontFamily="Arial"
+            >
+              {minutes}
+            </text>
+          </clipPath>
+        </defs>
+      </svg>
+      <div
+        className="text-9xl font-bold"
+        style={{
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          backgroundClip: "text",
+          color: "transparent",
+          lineHeight: 0.69,
+          paddingTop: "0em",
+          paddingBottom: "0.05em",
+          backgroundImage: `linear-gradient(0deg, #007bff ${percentage}%, #f8f9fa ${percentage}%)`,
+        }}
+      >
+        {minutes}
+      </div>
     </div>
   );
 };
