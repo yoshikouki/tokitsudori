015b21e8e4996570f65943edfd14e4d893cec477
yoshikouki <yoshikouki@gmail.com>
2024-02-13 23:26:56 +0900

    Update vite.config.ts and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import build from '@hono/vite-cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     build(),
     devServer({
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+      entry: "src/index.tsx",
+    }),
+    tsconfigPaths(),
+  ],
+});
