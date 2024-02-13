1bb5230773a737aee5458a8011b963f1369ea3fa
yoshikouki <yoshikouki@gmail.com>
2024-02-14 00:17:03 +0900

    Revert to index.htm
    This reverts commit ba94e36029f59b9046ae4d003163d8f56e1f004c.
    
    Revert "Rebuild with hono builder"
    
    This reverts commit 347c5eb713af9bc7abc28f4bea9c4f8b184dd033.
    
    Revert "Update vite.config.ts and package.json"
    
    This reverts commit 015b21e8e4996570f65943edfd14e4d893cec477.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import build from "@hono/vite-cloudflare-pages";
-import devServer from "@hono/vite-dev-server";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      entry: "src/index.tsx",
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
